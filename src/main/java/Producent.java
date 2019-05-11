704db1139d2ade60cd5c44b07a16c4fd722d6fbd
Mateusz Fiolek <matgithub89@op.pl>
2019-05-11 13:57:07 +0200

    git ignore 2

---
+++ src/main/java/Producent.java
@@ -0,0 +1,21 @@
+public class Producent {
+    private String marka;
+    private String model;
+    private String kolor;
+    private int moc;
+    private int pojemnosc;
+
+    public Producent(String marka, String model, String kolor, int moc, int pojemnosc) {
+        this.marka = marka;
+        this.model = model;
+        this.kolor = kolor;
+        this.moc = moc;
+        this.pojemnosc = pojemnosc;
+    }
+
+    public  String  toString(){
+
+      return "wyprodukowano samochod "+this.marka+" ,model "+this.model+" ,kolor "+this.kolor+" ,";
+    }
+
+}
